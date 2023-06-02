6227258eafa74a95547e96b28b2f3c1e389d4e89
Ravindu Senarathna <80905394+MrRevengerX@users.noreply.github.com>
2023-06-02 21:25:08 +0530

    Update stats.jsx

--- src/components/stats.jsx
+++ src/components/stats.jsx
@@ -2,19 +2,25 @@ import React from "react";
 
 function Stats() {
   return (
-    <div className="bg-nethraBlue min-h-[30vh]">
-      <div>
+    <div className="bg-nethraBlue min-h-fit flex items-center justify-center capitalize py-10">
+      <div className="flex gap-10 text-center text-white flex-wrap justify-center md:gap-16 lg:gap-60">
         <div>
-          <h1>18+</h1>
-          <p>Years of trust</p>
+          <h1 className="text-4xl lg:mb-1">18+</h1>
+          <p className="text-xl font-light lg:text-lg lg:font-normal">
+            Years of trust
+          </p>
         </div>
         <div>
-          <h1>12+</h1>
-          <p>expert doctors</p>
+          <h1 className="text-4xl lg:mb-1">12+</h1>
+          <p className="text-xl font-light lg:text-lg lg:font-normal">
+            expert doctors
+          </p>
         </div>
         <div>
-          <h1>10,000+</h1>
-          <p>Patients</p>
+          <h1 className="text-4xl lg:mb-1">10,000+</h1>
+          <p className="text-xl font-light lg:text-lg lg:font-normal">
+            Patients
+          </p>
         </div>
       </div>
     </div>
